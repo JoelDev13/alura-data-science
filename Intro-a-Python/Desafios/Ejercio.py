3a5c3884aa0b86e9a98099f78aa3a15bca51b454
JoelDev13 <joel13r11@gmail.com>
2025-11-08 23:31:16 -0400

    feat: primer commit

---
+++ Intro-a-Python/Desafios/Ejercio.py
@@ -0,0 +1,17 @@
+#1 - Imprime la frase "Escuela de Datos de Alura"
+print("Escuela de Datos de Alura");
+
+"""
+2 - Imprime tu primer nombre letra por letra. Por ejemplo, si mi nombre es Álvaro, la salida sería:
+
+Á
+L
+V
+A
+R
+O
+"""
+print("J");
+print("O");
+print("E");
+print("L");
