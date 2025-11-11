cfce064b61cfb751c514854c4cbc93ba02446c1b
JoelDev13 <joel13r11@gmail.com>
2025-11-11 01:05:42 -0400

    feat: agregando ejercios

---
+++ Intro-a-Python/Desafios/Ejercio3.py
@@ -0,0 +1,7 @@
+""" Crea un programa que solicite al usuario que escriba su nombre y edad, y luego imprima "Hola, [nombre], 
+tienes [edad] años """
+
+nombre = input("Ingrese su nombre")
+edad = int(input("Ingrese su edad"))
+
+print(f" Hola {nombre} tiene {edad} años")
---
+++ Intro-a-Python/Desafios/Ejercio4.py
@@ -0,0 +1,11 @@
+"""
+Crea un programa que solicite dos valores numéricos, un operador y una potencia, y 
+realice la exponenciación entre estos dos valores
+"""
+
+numero1 = int(input("numero 1"))
+numero2 = int(input("numero 2"))
+
+calculo = numero1 ** numero2
+
+print(calculo) 
